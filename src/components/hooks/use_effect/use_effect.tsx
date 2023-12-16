4017b4fd61b5efafdeb8149a22ed137bfa4e4b68
Amier Ali <amier_1998@hotmail.com>
2023-12-16 15:34:37 +0000

    feat: Implement Useffect with ToDo

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
+
+//"https://jsonplaceholder.typicode.com/todos/1"
+
 // make an api call using useEffect
+interface ToDo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const APICall = () => {
+  const [json, setJson] = useState<ToDo | null>(null);
+  useEffect(() => {
+    const fetchToDo = async () => {
+      // Your code here!
+      try {
+        const result = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/1"
+        );
+        const data = await result.json();
+        setJson(data);
+      } catch (error) {}
+    };
+    fetchToDo();
+  }, []);
 
-    // Your code here!
-
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+  return (
+    <div>
+      <h2>useEffect</h2>
+      <p>{json?.id}</p>
+      <p>{json?.title}</p>
+      <p>{json?.completed}</p>
+      <p>{json?.userId}</p>
+    </div>
+  );
+};
